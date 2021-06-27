d37dcf54ce62e5007c47fe2fbbbd7435a4470ff9
rakeshpriyad <rakeshpriyad@gmail.com>
2021-06-27 09:05:58 +0530

    css

---
+++ HTML/index.js
@@ -0,0 +1,10 @@
+let add = () => {
+    let x =document.getElementById("x").value;
+    let y =document.getElementById("y").value;
+    console.log(x);
+    let z =x+y;
+    console.log(z);
+}
+//console.log(add(10, 20));
+
+//let p = v => console.log(p);
